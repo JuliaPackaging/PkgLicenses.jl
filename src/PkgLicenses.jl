57e55204c8f9f0ab8e59fb65d925984788c12524
staticfloat@gmail.com <staticfloat@gmail.com>
2018-08-09 03:53:00 -0700

    Upgrade `PkgLicenses` to be 1.0-compatible

--- src/PkgLicenses.jl
+++ src/PkgLicenses.jl
@@ -19,7 +19,7 @@ const LICENSES = Dict(
 "Read license text from specified file and location"
 function readlicense(lic::AbstractString,
                      dir::AbstractString=normpath(dirname(@__FILE__), "..", "res", "licenses"))
-    return readstring(joinpath(dir, lic))
+    return read(joinpath(dir, lic), String)
 end
 
 """
